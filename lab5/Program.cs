346ce8c7f457fd95fd00fcf424eb5fedd33565b7
nayavu <nastyaxleborob@gmail.com>
2023-11-07 22:14:16 +0200

    Add project files.

---
+++ lab5/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+// Абстрактний клас Parent, що описує платника податків
+public abstract class Parent
+{
+    public abstract string Info(); // Абстрактний метод для повернення інформації про об'єкт
+    public abstract double Metod(); // Абстрактний метод для обчислення податку
+}
+
+// Дочірній клас Child1, що описує фізичну особу
+public class Child1 : Parent
+{
+    private double pole1; // загальний дохід
+    private double pole2; // ставка податку (%)
+
+    // Конструктор з двома параметрами
+    public Child1(double income, double taxRate)
+    {
+        pole1 = income;
+        pole2 = taxRate;
+    }
+
+    public override string Info()
+    {
+        return $"Фізична особа: Загальний дохід - {pole1}, Податкова ставка - {pole2}%";
+    }
+
+    public override double Metod()
+    {
+        return pole1 * pole2 / 100; // Обчислення податку
+    }
+}
+
+// Дочірній клас Child2, що описує юридичну особу
+public class Child2 : Parent
+{
+    private double pole3; // виручка від продажів
+    private double pole4; // собівартість
+    private double pole5; // додаткові витрати
+    private double pole6; // ставка податку (%)
+
+    // Конструктор з чотирма параметрами
+    public Child2(double salesRevenue, double costPrice, double additionalExpenses, double taxRate)
+    {
+        pole3 = salesRevenue;
+        pole4 = costPrice;
+        pole5 = additionalExpenses;
+        pole6 = taxRate;
+    }
+
+    public override string Info()
+    {
+        return $"Юридична особа: Виручка - {pole3}, Собівартість - {pole4}, Додаткові витрати - {pole5}, Податкова ставка - {pole6}%";
+    }
+
+    public override double Metod()
+    {
+        double profit = pole3 - pole4 - pole5; // Обчислення прибутку
+        return profit * pole6 / 100; // Обчислення податку
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Random rnd = new Random();
+
+        for (int i = 0; i < 5; i++)
+        {
+            // Випадковим чином визначаємо тип платника податків
+            bool isChild1 = rnd.Next(0, 2) == 0;
+
+            if (isChild1)
+            {
+                // Якщо особа фізична, визначаємо випадкові значення
+                double income = rnd.Next(1000, 10000);
+                double taxRate = rnd.Next(5, 30);
+
+                Child1 person1 = new Child1(income, taxRate);
+                Console.WriteLine(person1.Info());
+                Console.WriteLine("Податок: " + person1.Metod());
+            }
+            else
+            {
+                // Якщо особа юридична, визначаємо випадкові значення
+                double salesRevenue = rnd.Next(5000, 50000);
+                double costPrice = rnd.Next(3000, 20000);
+                double additionalExpenses = rnd.Next(1000, 5000);
+                double taxRate = rnd.Next(5, 25);
+
+                Child2 person2 = new Child2(salesRevenue, costPrice, additionalExpenses, taxRate);
+                Console.WriteLine(person2.Info());
+                Console.WriteLine("Податок: " + person2.Metod());
+            }
+        }
+        Console.ReadLine();
+    }
+}
---
+++ lab5_2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+public interface IParent
+{
+    string Info();
+    double Metod1();
+    double Metod2();
+}
+
+public class Child1 : IParent
+{
+    private double pole1;
+    private double pole2;
+    private double pole3;
+
+    public Child1(double a, double b, double c)
+    {
+        pole1 = a;
+        pole2 = b;
+        pole3 = c;
+    }
+
+    public string Info()
+    {
+        return $"Паралелепіпед: a={pole1}, b={pole2}, c={pole3}";
+    }
+
+    public double Metod1()
+    {
+        return 2 * (pole1 * pole2 + pole2 * pole3 + pole3 * pole1);
+    }
+
+    public double Metod2()
+    {
+        return pole1 * pole2 * pole3;
+    }
+}
+
+public class Child2 : IParent
+{
+    private double pole4;
+    private double pole5;
+
+    public Child2(double radius, double height)
+    {
+        pole4 = radius;
+        pole5 = height;
+    }
+
+    public string Info()
+    {
+        return $"Конус: Радіус={pole4}, висота={pole5}";
+    }
+
+    public double Metod1()
+    {
+        double l = Math.Sqrt(pole4 * pole4 + pole5 * pole5);
+        return Math.PI * pole4 * (pole4 + l);
+    }
+
+    public double Metod2()
+    {
+        return (Math.PI * pole4 * pole4 * pole5) / 3;
+    }
+}
+
+public class Child3 : IParent
+{
+    private double pole6;
+
+    public Child3(double radius)
+    {
+        pole6 = radius;
+    }
+
+    public string Info()
+    {
+        return $"Сфера: Радіус={pole6}";
+    }
+
+    public double Metod1()
+    {
+        return 4 * Math.PI * pole6 * pole6;
+    }
+
+    public double Metod2()
+    {
+        return (4 * Math.PI * pole6 * pole6 * pole6) / 3;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        for (int y = 0; y < 5; y++)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                IParent obj = null;
+
+                int shapeType = rnd.Next(1, 4);
+
+                if (shapeType == 1)
+                {
+                    double a = rnd.Next(1, 50);
+                    double b = rnd.Next(1, 50);
+                    double c = rnd.Next(1, 50);
+                    obj = new Child1(a, b, c);
+                }
+                else if (shapeType == 2)
+                {
+                    double radius = rnd.Next(1, 50);
+                    double height = rnd.Next(1, 50);
+                    obj = new Child2(radius, height);
+                }
+                else if (shapeType == 3)
+                {
+                    double r = rnd.Next(1, 50);
+                    obj = new Child3(r);
+                }
+
+                Console.WriteLine(obj.Info());
+                Console.WriteLine($"S={obj.Metod1().ToString("F", System.Globalization.CultureInfo.InvariantCulture)} V={obj.Metod2().ToString("F", System.Globalization.CultureInfo.InvariantCulture)}");
+                Console.WriteLine();
+            }
+                
+            Console.ReadLine();
+        }
+    }
+}
